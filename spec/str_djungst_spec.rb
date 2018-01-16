11b1538b8b2335c0ad59aa857980fb83926a3c81
David Jungst <djungst@gmail.com>
2018-01-16 10:20:46 -0700

    add something useful

--- spec/str_djungst_spec.rb
+++ spec/str_djungst_spec.rb
@@ -1,9 +1,23 @@
 RSpec.describe StrDjungst do
+  subject { StrDjungst }
+
   it "has a version number" do
     expect(StrDjungst::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y   S t r i n g')
+    end
   end
+
 end
+
+
+
